54858b236b1424b5b1fa643d397776cb084485cf
nostalgiccat <nesdom27@gmail.com>
2024-05-16 15:38:12 -0700

    added exception handling example code

--- main.py
+++ main.py
@@ -76,3 +76,28 @@ add_button.grid(row=4, column=1, columnspan=2)
 
 
 window.mainloop()
+
+# Error handling
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["sdfsdf"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    raise KeyError("This is an error that I made up")
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
